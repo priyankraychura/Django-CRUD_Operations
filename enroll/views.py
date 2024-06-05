76cc8b90ccf82a04b0972ebe50e1d0fc962fdbbf
priyankraychura <priyankrkaychura@gmail.com>
2024-06-05 17:43:08 +0530

    Django CRUD project added

---
+++ enroll/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, HttpResponseRedirect
+from .forms import StudentRegistration
+from .models import User
+
+# Create your views here.
+def add_show(request):
+    if request.method == 'POST':
+        fm = StudentRegistration(request.POST)
+        if fm.is_valid():
+            nm = fm.cleaned_data['name']
+            em = fm.cleaned_data['email']
+            pw = fm.cleaned_data['password']
+            reg = User(name=nm, email=em, password=pw)
+            reg.save()
+            # fm.save()
+            fm = StudentRegistration()
+    else:
+        fm = StudentRegistration()
+        
+    stud = User.objects.all()
+
+    return render(request, 'enroll/addAndShow.html', {'form': fm, 'stud': stud})
+
+def update_data(request, id):
+    if request.method == 'POST':
+        pi = User.objects.get(pk=id)
+        fm = StudentRegistration(request.POST, instance=pi)
+
+        if fm.is_valid():
+            fm.save()
+            return HttpResponseRedirect('/')
+    else:
+        pi = User.objects.get(pk=id)
+        fm = StudentRegistration(instance=pi)
+
+    return render(request, 'enroll/updateStudent.html', {'form': fm})
+
+def delete_data(request, id):
+    if request.method == 'POST':
+        pi = User.objects.get(pk=id)
+        pi.delete()
+        return HttpResponseRedirect('/')
